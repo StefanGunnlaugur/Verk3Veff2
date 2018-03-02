bec3bd1bc3eaf9150965a5ba7bf3c30e400528a2
StefanGunnlaugur <StefanGunnlaugur@gmail.com>
2018-03-02 03:06:22 +0000

    nr 3

--- api.js
+++ api.js
@@ -28,7 +28,10 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const result = await readAll();
-  res.json(result);
+  if (result) {
+    return res.json(result);
+  }
+  return res.status(404).json({ error: 'Not found' });
 });
 
 router.get('/:id', async (req, res) => {
